ff622631df8d82a298f0456baac336e47dc81732
Spring.Zhou <spring.zhou@samesamechina.com>
2015-06-11 11:46:12 +0800

    table php

---
+++ table.php
@@ -0,0 +1,51 @@
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="shortcut icon" type="image/ico" href="http://www.datatables.net/favicon.ico">
+  <meta name="viewport" content="initial-scale=1.0, maximum-scale=2.0">
+
+  <title>Louis Vuitton</title>
+  <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css">
+  <link rel="stylesheet" type="text/css" href="css/shCore.css">
+  <link rel="stylesheet" type="text/css" href="css/demo.css">
+  <script type="text/javascript" language="javascript" src="js/jquery.js"></script>
+</head>
+
+<body class="dt-example">
+  <div class="container">
+    <section>
+      
+     
+        <table id="example" class="display dataTable no-footer" cellspacing="0" width="100%" role="grid" aria-describedby="example_info" style="width: 100%;">
+        <thead>
+          <tr role="row">
+            <th class="sorting_asc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-sort="ascending" aria-label="PHOTO: activate to sort column descending" style="width: 100px;">ID</th>
+            <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="DOWNLOAD: activate to sort column ascending" style="width: 100px;">Name</th>
+            <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="NAME: activate to sort column ascending" style="width: 100px;">Mobile</th>
+            <th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" aria-label="FUNCTION: activate to sort column ascending" style="width: 100px;">Address</th>
+            
+          </tr>
+        </thead>
+
+        <tbody>
+                    
+        
+          <tr  role="row" >
+            <td align="center">1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+            <td align="center">4</td>
+  
+          </tr>
+          
+
+       </tbody>
+      </table>
+       
+    </section>
+  </div>
+
+  
+
+</body></html>
