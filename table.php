fd8859b25933dc718193e188f422dd89a35bdeb2
Spring.Zhou <spring.zhou@samesamechina.com>
2015-06-15 11:51:31 +0800

    page

--- table.php
+++ table.php
@@ -38,10 +38,51 @@
             <td align="center">4</td>
   
           </tr>
+          <tr  role="row" >
+            <td align="center">1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+            <td align="center">4</td>
+  
+          </tr>
+          <tr  role="row" >
+            <td align="center">1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+            <td align="center">4</td>
+  
+          </tr>
+          <tr  role="row" >
+            <td align="center">1</td>
+            <td align="center">2</td>
+            <td align="center">3</td>
+            <td align="center">4</td>
+  
+          </tr>
           
 
        </tbody>
       </table>
+      <div class="pageone">
+        <a href="" class="border"><<</a>
+        <a href="" class="border">1</a>
+        <a href="" class="border">2</a>
+        <a href="" class="disable">3</a>
+        <a href="" class="border">4</a>
+        <a href="" class="border">5</a>
+        <a href="" class="border">>></a>
+      </div>
+      <div class="pagetwo">
+        <a href="" class="disable"><<</a>
+        <a href="" class="border">1</a>
+        <a href="" class="border">2</a>
+        <a href="" class="border">3</a>
+        <a href="" class="border">4</a>
+        <a href="" class="etc">...</a>
+        <a href="" class="border">99</a>
+        <a href="" class="border">100</a>
+        <a href="" class="border">>></a>
+      </div>
        
     </section>
   </div>
